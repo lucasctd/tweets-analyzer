4b5c197f4be08220e4b4d92e70a688427920fe13
Lucas Reis <lucas@programmer.com.br>
2018-07-01 21:03:10 -0300

    Atualizar view de sentimentos
    A view relacionada a busca de sentimentos foi atualizada conforme nova modelagem do banco de dados.

--- scripts/v_sentimentos.sql
+++ scripts/v_sentimentos.sql
@@ -2,4 +2,11 @@ CREATE OR REPLACE VIEW V_SENTIMENTOS AS
 SELECT sent.score, sent.magnitude, tw.precandidato_id, tw.tweet_created_at as tweet_criado_em FROM sentiment sent
 INNER JOIN tweet tw ON sent.id = tw.sentiment_id;
 
-SELECT * FROM V_SENTIMENTOS;
+SELECT * FROM V_SENTIMENTOS where precandidato_id = 2 ORDER BY tweet_criado_em;
+
+SELECT score, DATE_FORMAT(tweet_criado_em,'%d/%m/%Y %H:%i') AS criado_em  
+FROM V_SENTIMENTOS WHERE precandidato_id = 2
+ORDER BY tweet_criado_em
+
+
+DATE_FORMAT(tweet_criado_em,'%d/%m/%Y %H:%i') AS tweet_criado_em 

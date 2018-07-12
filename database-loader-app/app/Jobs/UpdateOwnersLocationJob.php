8d714a01eee92c272425bb1d8d7af7cacb0a46df
Lucas Reis <lucas@programmer.com.br>
2018-07-11 23:59:05 -0300

    Implementar Job Interface
    A interface de job foi implementada e alguns ajustes de código foram feitos de forma a otimizá-lo e tornar as mensagens de status mais claras.

--- database-loader-app/app/Jobs/UpdateOwnersLocationJob.php
+++ database-loader-app/app/Jobs/UpdateOwnersLocationJob.php
@@ -14,11 +14,21 @@ use App\Models\City;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use App\Exceptions\AppException;
+use App\Traits\LocationTrait;
+use App\Interfaces\JobInterface;
 
-
-class UpdateOwnersLocationJob implements ShouldQueue
+/**
+ * Classe resposável pela busca dos Tweets
+ *
+ * @category Job
+ * @package  App\Jobs
+ * @author   Lucas Reis <lucas@programmer.com.br>
+ * @license  https://github.com/lucasctd/tweets-analyzer/blob/master/LICENSE - LICENSE
+ * @link     https://github.com/lucasctd/tweets-analyzer
+ */
+class UpdateOwnersLocationJob implements ShouldQueue, JobInterface
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, LocationTrait;
 
     /**
      * Create a new job instance.
@@ -40,62 +50,94 @@ class UpdateOwnersLocationJob implements ShouldQueue
         $owners = TweetOwner::all();
         $totalAtualizado = 0;
         $totalNaoAtualizado = 0;
+        $total = count($owners);
+        $percentual = 0;
+        $totalAnalisado = 0;
         DB::beginTransaction();
-        try{
+        try {
             foreach ($owners as $owner) {
-                $location = $this->getLocation($owner->location);
-                $city = $this->getCity($location[0]);
+                $location = $this->_getLocation($owner->location);
+                $city = $this->_getCity($location[0]);
                 $state = $this->getState($location[0]);
                 $cityName = 'NE';
                 $stateName = 'NE';
-                if($city != null){
+                if ($city != null) {
                     $owner->city_id = $city->codigo;
                     $cityName = $city->nome;
                 }
-                if($state != null){
+                if ($state != null) {
                     $owner->br_state_id = $state->codigo;
                     $stateName = $state->nome;
                 }
-                if($city != null || $state != null){
+                $novoPercentual = $this->_calcPercentual(++$totalAnalisado, $total);
+                if ($novoPercentual > $percentual) {
+                    $percentual = $novoPercentual;
+                }
+                if ($city != null || $state != null) {
                     $owner->save();
                     $totalAtualizado+=1;
-                    event(new UpdateOwnersLocationStatusEvent($owner->name . ' teve sua localização atualizada para ' . $cityName . '/' . $stateName));
-                }else{
+                    $this->fireEvent($owner->name . ' teve sua localização atualizada para ' . $cityName . '/' . $stateName .'{'.$percentual.'}');
+                } else {
                     $totalNaoAtualizado+=1;
-                    event(new UpdateOwnersLocationStatusEvent($owner->name . " não foi atualizado, pois não foi encontrado registro de cidade ou estado para a localização 
-                    informada ($location[0])")); 
+                    $this->fireEvent(
+                        $owner->name . " não foi atualizado, pois não foi encontrado registro de cidade ou estado para a localização informada ($location[0])".'{'.$percentual.'}'
+                    );
                 }
             }
-            event(new UpdateOwnersLocationStatusEvent("Análise finalizadam, $totalAtualizado registros foram atualizados e $totalNaoAtualizado não tiveram sua localização identificada.")); 
+            $this->fireEvent("Análise finalizada, $totalAtualizado registros foram atualizados e $totalNaoAtualizado não tiveram sua localização identificada.");
             DB::commit();
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             DB::rollBack();
-            event(new UpdateOwnersLocationStatusEvent('Ocorreu um erro, alteração foi persistida, favor verificar o log para detalhes.'));
+            $this->fireEvent('Ocorreu um erro, as alterações não foram persistidas, favor verificar o log para detalhes.');
             Log::error($e->getMessage());
             Log::error(AppException::getTraceAsString($e));
         }
-        
     }
 
-    private function getCity($cityName) : ?City {
-        $city = City::where('nome', $cityName)->get();
-        return count($city) === 1 ? $city->first() : null;
-    }
-
-    private function getState($cityName) : ?State {
-        $state = State::where('nome', $cityName)->get();
-        return $state->isNotEmpty() ? $state->first() : null;
-    }
-
-    private function getLocation($ownerLocation): array {
+    /**
+     * Execute the job.
+     *
+     * @param string $ownerLocation - Localização do usuário conforme Twitter API
+     *
+     * @link https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/user-object
+     *
+     * @return void
+     */
+    private function _getLocation(string $ownerLocation): array
+    {
         $location = explode(',', $ownerLocation);
-        if(count($location) === 1){
+        if (count($location) === 1) {
             $location = explode('-', $ownerLocation);
         }
 
-        if(count($location) === 1){
+        if (count($location) === 1) {
             $location = explode('/', $ownerLocation);
         }
         return $location;
+    }
+
+    /**
+     * Calcular percentual de $value em relação a $total
+     *
+     * @param int $value - Autoexplicativo
+     * @param int $total - Autoexplicativo
+     *
+     * @return int - Percentual (inteiro) do valor informado
+     */
+    private function _calcPercentual(int $value, int $total) : int
+    {
+        return ($value * 100) / $total;
+    }
+
+    /**
+     * Dispara o evento de status do Job
+     *
+     * @param string $status - Status do Job
+     *
+     * @return void
+     */
+    public function fireEvent(string $status) : void
+    {
+        event(new UpdateOwnersLocationStatusEvent($status));
     }
 }

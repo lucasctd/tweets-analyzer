4609ca6c03cc6dce50bc24b2e4a6e32c2a846b7f
Lucas Reis <lucas@programmer.com.br>
2018-07-11 23:48:18 -0300

    Criar interfaces para Job e Filter
    A nova interface "FilterInterface" é o contrato para a utilização de novos filtros na aplicação;
    A interface "JobInterface" deve ser utilizada em todos os jobs da aplicação.

---
+++ database-loader-app/app/Interfaces/JobInterface.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Interfaces;
+
+use \Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * Interface obrigatória nos jobs da aplicação
+ *
+ * @category Interface
+ * @package  App\Interfaces
+ * @author   Lucas Reis <lucas@programmer.com.br>
+ * @license  https://github.com/lucasctd/tweets-analyzer/blob/master/LICENSE - LICENSE
+ * @link     https://github.com/lucasctd/tweets-analyzer
+ */
+interface JobInterface
+{
+    /**
+     * Dispara evento do job
+     *
+     * @param string $status - Status do Job
+     *
+     * @return void
+     */
+    public function fireEvent(string $status) : void;
+}

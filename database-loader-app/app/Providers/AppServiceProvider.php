543a541e87c438fc9fca909d8d825385e987f024
Lucas Reis <lucas@programmer.com.br>
2018-07-12 00:26:28 -0300

    Adicionar implementação de filtro padrão
    Adicionada implementação de filtro padrão da aplicação, que no caso chama-se "Precandidado".

--- database-loader-app/app/Providers/AppServiceProvider.php
+++ database-loader-app/app/Providers/AppServiceProvider.php
@@ -1,12 +1,32 @@
 <?php
-
+// phpcs:ignore
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-
+use App\Interfaces\FilterInterface;
+use App\Models\PreCandidato;
+// phpcs:ignore
 class AppServiceProvider extends ServiceProvider
 {
+
+    /**
+     * All of the container bindings that should be registered.
+     *
+     * @var array
+     */
+    public $bindings = [
+        FilterInterface::class => PreCandidato::class,
+    ];
+
+    /**
+     * All of the container singletons that should be registered.
+     *
+     * @var array
+     */
+    public $singletons = [
+    ];
+
     /**
      * Bootstrap any application services.
      *

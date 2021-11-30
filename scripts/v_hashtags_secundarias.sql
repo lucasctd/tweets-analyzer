878688d659d491f9a65d997c1cc5097da46dfc41
Lucas Reis <lucas@programmer.com.br>
2021-11-30 15:44:23 -0300

    remove SQL Scripts, queries will be done through Laravel framework, so no need to use SQL scripts anymore

--- scripts/v_hashtags_secundarias.sql
+++
@@ -1,5 +0,0 @@
-SELECT ht.name FROM hashtag ht
-INNER JOIN tweet tw ON ht.tweet_id = tw.id
-WHERE ht.name NOT IN (
-	SELECT sub_ht.name FROM hashtag sub_ht WHERE sub_ht.filter_id = 1 AND sub_ht.primary = '1'
-) AND tw.filter_id = 1;
